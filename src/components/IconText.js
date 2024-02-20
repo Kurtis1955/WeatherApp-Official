750201185ec66839e388fc033f281b8b573a08b0
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2024-02-20 15:32:17 -0800

    Create IconText.js

---
+++ src/components/IconText.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View,Text,StyleSheet } from 'react-native'
+import {Feather} from '@expo/vector-icons'
+
+const IconText = () => {
+  return (
+   <View>
+
+   </View> 
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default IconText
