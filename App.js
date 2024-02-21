366e5ea3207ae27a034867042e81a29b9357e055
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2024-02-21 14:38:19 -0800

    Update App.js

--- App.js
+++ App.js
@@ -3,12 +3,16 @@ import { View,StyleSheet } from 'react-native';
 import CurrentWeather from './src/screens/CurrentWeather';
 import UpcomingWeather from './src/screens/UpcomingWeather';
 import City from './src/screens/City';
+import { NavigationContainer } from '@react-navigation/native';}
 
 const App = () =>{
   return(
-    <View style={styles.container}>
-      <CurrentWeather/>
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <CurrentWeather/>
+      </View>
+    </NavigationContainer>
+
   )
 }
 
