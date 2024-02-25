d5b58942c082b2a1b221d3583b98b8657362eb77
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2024-02-25 15:49:26 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,52 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
+import { ActivityIndicator, View, StyleSheet} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/components/Tabs';
 
 const App = () =>{
+  const [loading,setLoading]=useState(true)
+  const [location,setLocation] = useState(null)
+  const [error,setError] = useState(null)
+
+  useEffect(()=>{
+    // passing a funtion with immeadiate effect
+    (async ()=>{
+      let {status} = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted'){
+        setError('permission to access location denied')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({})
+      setLocation(location)
+    })()
+  }, 
+  // to immediately invoke the function we set an empty array 
+  [] )
+
+  if (location){
+    console.log(location)
+  }
+
+  if (loading){
+    return(
+      <View style={StyleSheet.container}>
+        <ActivityIndicator size={'large'} color={'blue'} />
+      </View>
+    )
+  }
+ 
   return(
     <NavigationContainer>
       <Tabs/>
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container:{
+    justifyContent:'center',
+    flex:1
+  }
+})
 
 export default App
