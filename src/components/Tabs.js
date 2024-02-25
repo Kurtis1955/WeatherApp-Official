b7fa55acbec77ac0c2ff872df55d46a11f738fad
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2024-02-25 13:35:56 -0800

    Create Tabs.js

---
+++ src/components/Tabs.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import CurrentWeather from './src/screens/CurrentWeather';
+import UpcomingWeather from './src/screens/UpcomingWeather';
+import City from './src/screens/City';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Feather } from '@expo/vector-icons';
+
+const Tab = createBottomTabNavigator()
+
+const Tabs = () => {
+  return (
+    
+  )
+}
+
+export default Tabs
