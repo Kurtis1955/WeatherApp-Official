e2764f7be538d6193abca41516dab317d0024002
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2024-02-25 14:34:28 -0800

    Create Counter.js

---
+++ src/demonstration/Counter.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react';
+import { View,Text, Button, StyleSheet} from 'react-native';
+
+const Counter = () => {
+    const[count,setCount]=useState(0)
+  return (
+    <View style={styles.container}>
+        <Text style={styles.title}>{`count: ${count}`}</Text>
+        <Button
+          color={'red'}
+          title={'increase the count'}
+          onPress={()=>setCount(count++)}
+        />
+        <Button   
+          color={'green'}
+          title={'decrease the count'}
+          onPress={()=>setCount(count--)}/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor:'orange'
+    },
+    title:{
+        alignSelf:'center',
+        fontSize:25,
+        marginTop:25
+    }    
+
+})
+
+export default Counter
