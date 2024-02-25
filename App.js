f24e7c631037f00242e2f8baf68f02df37837590
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2024-02-25 15:12:20 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './src/components/Tabs';
-import Counter from './src/demonstration/counter';
 
 const App = () =>{
   return(
     <NavigationContainer>
-      <Counter/>
+      <Tabs/>
     </NavigationContainer>
   )
 }
