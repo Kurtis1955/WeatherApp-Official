de1a4f69a699f30a79d890edf083981273f25b9e
Theophilus Akowuah <118230741+Kurtis1955@users.noreply.github.com>
2024-02-25 13:40:39 -0800

    Update Tabs.js

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import CurrentWeather from './src/screens/CurrentWeather';
-import UpcomingWeather from './src/screens/UpcomingWeather';
-import City from './src/screens/City';
+import CurrentWeather from '../screens/CurrentWeather';
+import UpcomingWeather from './screens/UpcomingWeather';
+import City from './screens/City';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Feather } from '@expo/vector-icons';
 
@@ -9,7 +9,33 @@ const Tab = createBottomTabNavigator()
 
 const Tabs = () => {
   return (
-    
+    <Tab.Navigator
+    screenOptions={{
+     tabBarActiveTintColor:"tomato",
+     tabBarInactiveTintColor:"grey"
+    }}>
+ <Tab.Screen name={'Current'} component={CurrentWeather} options={{
+   tabBarIcon:({focused}) => 
+     (<Feather 
+        name={'droplet'} 
+        size={25} 
+        color={focused ? 'tomato':'#000'}/>)
+ }} />
+ <Tab.Screen name={'Upcoming'} component={UpcomingWeather} options={{
+   tabBarIcon:({focused}) => 
+     (<Feather 
+        name={'clock'} 
+        size={25} 
+        color={focused ? 'tomato':'#000'}/>)
+ }}/>
+ <Tab.Screen name={'City'} component={City} options={{
+   tabBarIcon:({focused}) => 
+     (<Feather 
+        name={'home'} 
+        size={25} 
+        color={focused ? 'tomato':'#000'}/>)
+ }}/>
+ </Tab.Navigator>
   )
 }
 
